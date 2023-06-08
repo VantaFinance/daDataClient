d3ea454d731aa8ff0fba5c144710ae3c90702ca4
Vlad Shashkov <shashkov.root@gmail.com>
2023-06-08 18:59:54 +0700

    Fixed check version

--- src/Infrastructure/Composer/function.php
+++ src/Infrastructure/Composer/function.php
@@ -11,6 +11,7 @@ declare(strict_types=1);
 namespace Vanta\Integration\DaData\Infrastructure\Composer;
 
 use Composer\InstalledVersions;
+use Composer\Semver\Comparator;
 
 /**
  * @internal
@@ -26,76 +27,6 @@ function isOldPackage(string $package, string $version): bool
         throw new \RuntimeException(sprintf('Not found version package: %s', $package));
     }
 
-    return parsePackageVersion($packageVersion)->isOld(parsePackageVersion($version));
-}
+    return Comparator::greaterThan($version, $packageVersion);
 
-/**
- * @internal
- *
- * @psalm-suppress ArgumentTypeCoercion
- *
- * @param non-empty-string $version
- */
-function parsePackageVersion(string $version): Version
-{
-    $matches = [];
-
-    preg_match('/^(?<major>\d*).(?<minor>\d*)/m', $version, $matches);
-
-    if ([] == $matches) {
-        throw new \RuntimeException('Not found version package');
-    }
-
-    [ 'major' => $major , 'minor' => $minor] = $matches;
-
-    return new Version($major, $minor);
-}
-
-/**
- * @psalm-immutable
- *
- * @internal
- */
-final class Version
-{
-    /**
-     * @var numeric-string
-     */
-    private string $major;
-
-    /**
-     * @var numeric-string
-     */
-    private string $minor;
-
-    /**
-     * @param numeric-string $major
-     * @param numeric-string $minor
-     */
-    public function __construct(string $major, string $minor)
-    {
-        $this->major = $major;
-        $this->minor = $minor;
-    }
-
-    /**
-     * @return numeric-string
-     */
-    public function getMajor(): string
-    {
-        return $this->major;
-    }
-
-    /**
-     * @return numeric-string
-     */
-    public function getMinor(): string
-    {
-        return $this->minor;
-    }
-
-    public function isOld(self $version): bool
-    {
-        return $this->major <= $version->getMajor() && $this->minor <=> $version->getMinor();
-    }
 }
