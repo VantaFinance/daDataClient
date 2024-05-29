84aac4ce71782e144cae342e11f70a9654862839
Dmitry Kaufman <d.kaufman@pos-credit.ru>
2024-05-29 15:04:01 +0700

    Revert "ref #NB2-243 test types"
    This reverts commit 4b5c548190ae15ba26fa1c91ee4406e37d7534a1.

--- src/Response/OrganizationType.php
+++ src/Response/OrganizationType.php
@@ -19,19 +19,10 @@ use MyCLabs\Enum\Enum;
  */
 final class OrganizationType extends Enum
 {
-    /**
-     * @var string
-     */
     private const INDIVIDUAL = 'INDIVIDUAL';
 
-    /**
-     * @var string
-     */
     private const LEGAL = 'LEGAL';
 
-    /**
-     * @var string
-     */
     private const UNKNOWN = 'UNKNOWN';
 
     public static function individual(): self
