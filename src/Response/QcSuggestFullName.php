24f623e5c5e7b65efacfcf5397058282e5d7800c
Dmitry Kaufman <d.kaufman@pos-credit.ru>
2024-05-29 15:20:01 +0700

    ref #NB2-243 test types

--- src/Response/QcSuggestFullName.php
+++ src/Response/QcSuggestFullName.php
@@ -6,24 +6,49 @@ namespace Vanta\Integration\DaData\Response;
 
 use MyCLabs\Enum\Enum;
 
-/**
- * @psalm-immutable
- *
- * @extends Enum<numeric-string>
- */
-final class QcSuggestFullName extends Enum
-{
-    private const KNOWN = '0';
-
-    private const UNKNOWN = '1';
-
-    public static function known(): self
+if (version_compare(PHP_VERSION, '8.3.0')) {
+    /**
+     * @psalm-immutable
+     *
+     * @extends Enum<numeric-string>
+     */
+    final class QcSuggestFullName extends Enum
     {
-        return new self(self::KNOWN);
+        private string const KNOWN = '0';
+
+        private string const UNKNOWN = '1';
+
+        public static function known(): self
+        {
+            return new self(self::KNOWN);
+        }
+
+        public static function unknown(): self
+        {
+            return new self(self::UNKNOWN);
+        }
+    }
+} else {
+    /**
+     * @psalm-immutable
+     *
+     * @extends Enum<numeric-string>
+     */
+    final class QcSuggestFullName extends Enum
+    {
+        private const KNOWN = '0';
+
+        private const UNKNOWN = '1';
+
+        public static function known(): self
+        {
+            return new self(self::KNOWN);
+        }
+
+        public static function unknown(): self
+        {
+            return new self(self::UNKNOWN);
+        }
     }
 
-    public static function unknown(): self
-    {
-        return new self(self::UNKNOWN);
-    }
 }
