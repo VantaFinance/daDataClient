1db55b252a47423e701edf59f21947d0d44a5935
Dmitry Kaufman <kaufmandmitry@gmail.com>
2024-06-07 14:51:59 +0700

    ref #NB2-243 add managment in suggest organization response (#20)

---
+++ src/Response/Management.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vanta\Integration\DaData\Response;
+
+final class Management
+{
+    /**
+     * @var non-empty-string
+     */
+    private string $name;
+
+    /**
+     * @var non-empty-string|null
+     */
+    private ?string $post;
+
+    /**
+     * @param non-empty-string      $name
+     * @param non-empty-string|null $post
+     */
+    public function __construct(string $name, ?string $post)
+    {
+        $this->name = $name;
+        $this->post = $post;
+    }
+
+    /**
+     * @return non-empty-string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return non-empty-string|null
+     */
+    public function getPost(): ?string
+    {
+        return $this->post;
+    }
+}
